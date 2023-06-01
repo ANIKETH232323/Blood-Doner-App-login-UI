b51f6ba8a169797f50c0b3c7823334a051c567ec
Aniketh <aniketh5660@gmail.com>
2023-06-02 02:47:10 +0530

    Continuing..........

--- app/src/main/java/com/example/blood_bank_app/MainActivity.kt
+++ app/src/main/java/com/example/blood_bank_app/MainActivity.kt
@@ -5,18 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -24,16 +18,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.node.modifierElementOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.blood_bank_app.ui.theme.BloodBankAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Box(modifier = Modifier.background(Color.Red))
             FrontPage()
         }
     }
@@ -42,8 +35,12 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FrontPage(){
-    Box(modifier = Modifier.background(Color.Red))
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(Color.Red)
+            .fillMaxHeight()
+        ) {
         Image(painter = painterResource(id = R.drawable.r), contentDescription = "Blood")
         val tex = remember {
             mutableStateOf("")
@@ -57,8 +54,8 @@ fun FrontPage(){
                 Text(text = "Enter Your Name")
             },
             leadingIcon = {
-                Icon(imageVector = Icons.Filled.Email,
-                    contentDescription ="Email-icon" )
+                Icon(painter = painterResource(id =R.drawable.baseline_email_24),
+                    contentDescription = "Email")
             }
         )
 
